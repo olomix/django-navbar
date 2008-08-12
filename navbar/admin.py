9c4f4f87fa51ccfd8f1917ff6f6fc6bbabcf0b11
doug.napoleone <doug.napoleone@40ae0140-9c48-0410-9b1c-13f58143d509>
2008-08-12 16:44:53 +0000

    Actually I already had an admin.py in my local install, I just forgot to 'add' it. Sorry about that.
    This is the correct admin.py for the model change.
    
    
    
    git-svn-id: http://django-navbar.googlecode.com/svn/trunk@10 40ae0140-9c48-0410-9b1c-13f58143d509

--- navbar/admin.py
+++ navbar/admin.py
@@ -1,20 +1,16 @@
 from django.contrib import admin
-from models import NavBarEntry
+
+from navbar.models import NavBarEntry
 
 class NavBarEntryAdmin(admin.ModelAdmin):
-    """
-    Defines admin display and functionality for NavBarEntry.
-
-    """
-
-    fieldsets = (
-        (None, {'fields': ('name', 'title', 'url', 'order', 'parent')}),
-        ('Advanced Permissions', {'classes': 'collapse',
-                         'fields': ('user_type', 'groups', )}),
-    )
-    list_filter = ('parent',)
-    list_display = ('name', 'url', 'order', 'parent')
-    search_fields = ('url', 'name', 'title')
+        fieldsets = (
+            (None, {'fields': ('name', 'title', 'url', 'order', 'parent')}),
+            ('Advanced Permissions', {'classes': ('collapse',),
+                             'fields': ('path_type', 'user_type', 'groups', )}),
+        )
+        list_filter = ('parent',)
+        list_display = ('name', 'url', 'order', 'parent')
+        search_fields = ('url', 'name', 'title')
+        filter_horizontal = ("groups",)
 
 admin.site.register(NavBarEntry, NavBarEntryAdmin)
-
